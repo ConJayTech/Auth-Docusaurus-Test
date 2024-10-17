9fce45d8f788e2f25d50546daf1484c97e8a085b
ConJayTech <conor.teich@gmail.com>
2024-10-17 14:36:52 -0700

    Update docusaurus.config.js

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -149,7 +149,7 @@ const config = {
 export default {
   title: 'Docusaurus TEST',
   url: 'https://conjaytech.github.io',
-  baseUrl: '/Auth-Docusaurus-Test/',
+  baseUrl: '/Auth-Docusaurus-Test/my-website/intro.md',
   projectName: 'conjaytech.github.io',
   organizationName: 'Conor, IQM',
 };
